5160339bcaf8c77548cf2e982c9b86e13a77392c
Pralay <pralaydutta666.pd@gmail.com>
2024-12-14 10:55:14 +0530

    Made dynamic change possible in todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./styles.css"
+/* tslint:disable:no-unused-variable */
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
+  const [newItem,setNewItem]=useState("");//Define state for all the components for which you want the code to re-render
+  const [todos,setTodos]=useState([]);
+  
 
-  return (
+function handleSubmit(e){
+    e.preventDefault();
+    setTodos((currentTodos)=>{
+      return [
+        ...currentTodos,{
+          id:crypto.randomUUID(),
+          title:newItem,
+          completed:false
+        }
+      ]
+    })
+    setNewItem("");
+  }
+function handleDeleteItem(givenid){
+  // e.preventDefault;
+  setTodos((currentTodos)=>{
+    const filteredTodos=currentTodos.filter(todo=>todo.id!=givenid);
+    return [filteredTodos];
+  })
+}
+function toggleTodo(givenid,completed){
+  
+}
+
+console.log(todos);
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit} action="" className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input value={newItem} 
+          onChange={e=>setNewItem(e.target.value)} 
+          type="text" id="item" />
+          <button className="btn add-btn">Add</button>
+        </div>
+      </form> 
+      <h1 className="header">TODO List</h1>
+      <ul className="list">
+        {todos.map(todo=>{
+          return (
+            <li key={todo.id}>
+          <label htmlFor="">
+            <input type="checkbox" onChange={e =>{toggleTodo(todo.id,e.target.checked)}} checked={todo.completed}/>
+            {todo.title}
+          </label>
+          <button onClick={e=> handleDeleteItem(todo.id)} className="btn btn-danger">Delete</button>
+        </li>
+          )
+        })}
+        
+        
+      </ul>
     </>
   )
 }
-
-export default App
